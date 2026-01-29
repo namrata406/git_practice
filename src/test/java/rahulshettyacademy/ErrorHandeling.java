80920ffef6819268b1433c396bab849dd5012684
namrata <namrata4062021@gmail.com>
2026-01-29 21:01:00 +0530

    this is the first change

--- src/test/java/rahulshettyacademy/ErrorHandeling.java
+++ src/test/java/rahulshettyacademy/ErrorHandeling.java
@@ -8,6 +8,7 @@ import rahulshettyacademy.TestComponents.Retry;
 public class ErrorHandeling extends BaseTest {
 	@Test(retryAnalyzer = Retry.class)
 	public void ErrorLogin() {
+		System.out.println("Hey I am here");
 		landingPage.loginWithinPage("namrata4062021@gmail.com","Singh");
 	    driver.findElement(By.xpath("//button[@routerlink='/dashboard/cart']")).click();
 	    driver.close();
